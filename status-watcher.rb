b87130e5895300604d21ca04951024a08b3c0bd4
anekos <anekos@snca.net>
2018-06-17 11:15:24 +0900

    Add variables: duration, position, index, current, total

--- status-watcher.rb
+++ status-watcher.rb
@@ -12,33 +12,45 @@ load './lib/syno.rb'
 command_line = ARGV.dup
 
 
+class Integer
+  def to_time
+    '%d:%.2d' % [self / 60, self % 60]
+  end
+end
+
+
+
 Syno.new do
   |syno|
 
   previous = nil
 
   loop do
-    status = syno.audio_station.status
+    data = syno.audio_station.status['data']
 
-    song = status.dig('data', 'song')
+    song = data.dig('song')
 
-    current =
-      if song and additional = song.dig('additional', 'song_tag')
-        {
-          :album =>  additional.dig('album'),
-          :artist => additional.dig('artist') || additional.dig('album_artist'),
-          :title => song.dig('title'),
-        }
-      else
-        {
-          :album => '-', :artist => '-', :title => '-'
-        }
-      end
+    current = {
+      :state => data.dig('state'),
+      :total => data.dig('playlist_total'),
+      :index => data.dig('index'),
+      :current => data.dig('index').to_i + 1,
+      :position => data.dig('position').to_time,
+    }
+
+    if song and additional = song.dig('additional')
+      current.merge!({
+        :album =>  additional.dig('album'),
+        :artist => additional.dig('artist') || additional.dig('song_tag', 'album_artist'),
+        :title => song.dig('title'),
+        :duration => additional.dig('song_audio', 'duration').to_time,
+      })
+    end
 
     unless current == previous
       cmd = command_line.map do
         |it|
-        it.gsub(/%(:?artist|album|title)%/) {|it| current[it[1...-1].to_sym] } .shellescape
+        it.gsub(/%(:?artist|album|title|current|index|total|position|duration)%/) {|it| current[it[1...-1].to_sym] } .shellescape
       end.join(' ')
 
       system(cmd)
